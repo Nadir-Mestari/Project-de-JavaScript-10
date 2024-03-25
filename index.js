76dc4009c9378b43753aa36c4c9fc6167712884e
Nadir <contact@mestarico.com>
2024-03-25 18:18:42 +0100

    Finish

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 // Créer la logique de compte à rebours
 
-// choice.value = 5 minutes
+const form = document.querySelector("form")
 
-// choice.value * 60 = totalSeconds
+const countdownDisplay = document.getElementById("countdownDisplay")
+let totalsecondes;
+let interval;
+function Timer() {
 
-// let minutes = Math.floor(totalSeconds / 60);
-// let seconds = totalSeconds % 60
+    let minutes = Math.floor(totalsecondes / 60);
+    let secondes = totalsecondes % 60;
 
-// Créer un événement à la validation du form pour lancer le compte à rebours
+    let num1 = minutes < 10 ? "0" + minutes : minutes
+    let num2 = secondes < 10 ? "0" + secondes : secondes
+
+    countdownDisplay.innerText = `${num1}:${num2}`
+    if (totalsecondes > 0) {
+        totalsecondes--;
+    } else {
+        countdownDisplay.innerText = `C'est terminé !`
+        clearInterval(interval)
+    }
+
+
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (isNaN(choice.value)) {
+        alert("le miniteur contiens seulement des chiffres")
+    } else {
+        totalsecondes = choice.value * 60;
+        clearInterval(interval)
+        interval = setInterval(Timer, 1000);
+        choice.value = "";
+    }
+
+
+
+})
+
+
+
